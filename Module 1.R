ec34ac4ac22e6931651855693af431e3d721d1ea
David M. <d.mendsev@gmail.com>
2024-08-26 14:57:14 +0200

    mod1

---
+++ Module 1.R
@@ -0,0 +1,20 @@
+# Probability----
+## Chile ----
+rain <- -4 +5
+overcast <- -4 +5 
+sunny <- 6-5
+
+# Bayes' theorem---- 
+pass <- 203+118+178+212+122+167+528+673
+p  red   blue
+A  2     3
+B  5     1
+C  3
+
+##6
+3/5/3+1/6/3
+3/3*1/3/((2+5+3)/(3+1+2+5+3))
+# Review of distributions ----
+E[xy - xEy - yEx + ExEy]
+# Probability and Bayes' Theorem ----
+-1e(-3x)
